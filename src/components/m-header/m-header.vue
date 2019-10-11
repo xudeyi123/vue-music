54e627c1572b7fb4aa9431fb57b4f2e826e1339f
xudeyi <519301265@qq.com>
2019-10-11 16:50:22 +0800

    feat: 完成header模块

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,6 +1,7 @@
 <template>
     <div class='m-header'>
-
+        <div class="icon"></div>
+        <h1 class="text">动感 Music</h1>
     </div>
 </template>
 
@@ -11,5 +12,33 @@
 </script>
 
 <style scoped lang="less">
+    @import "../../common/less/variable.less";
+    @import "../../common/less/mixin.less";
 
+    .m-header {
+        position:relative;
+        height:44px;
+        text-align :center;
+        color :@color-theme;
+        font-size :0
+    }
+    
+    .icon {
+        display :inline-block;
+        vertical-align :top;
+        margin-top:6px;
+        width:30px;
+        height :32px;
+        margin-right:9px;
+        .bg-image('./img/logo');
+        background-size :30px 32px;
+    }
+      
+    .text { 
+        display :inline-block;
+        vertical-align :top;
+        line-height :44px;
+        font-size:@font-size-large;
+    }
+      
 </style>
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class='tab'>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped lang='less'>
+    @import '../../common/less/variable.less';
+    .tab {
+        display :flex;
+        height :44px;
+        line-height:44px;
+        font-size : @font-size-medium
+    }
+    
+    .tab-item {
+        flex:1;
+        text-align:center;
+        .tab-link {
+            padding-bottom :5px;
+            color: @color-text-1
+        }
+        &.router-link-active {
+            .tab-link {
+                color : @color-theme;
+                border-bottom :2px solid @color-theme
+            }           
+        }         
+    }
+      
+</style>
