d3a30d76172fb33ce193867894e2bd1d32111cb9
xudeyi <519301265@qq.com>
2019-10-12 15:04:57 +0800

    feat: tab模块与router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './common/less/index.less'
+import fastclick from 'fastclick'
+import router from './router'
+
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
