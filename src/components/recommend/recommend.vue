3e273d4463c7008fe2800cb78d7e2f2f96840223
xudeyi <519301265@qq.com>
2019-10-15 17:05:10 +0800

    feat: 轮播图完成

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,15 +1,65 @@
 <template>
-    <div>
-        推荐
+  <div class="recommend">
+    <div class="recommend-content">
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends" :key="item.id">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl" />
+            </a>
+          </div>
+        </slider>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+import { getRecommend } from "../../api/recommend";
+import { ERR_OK } from "../../api/config";
+import Slider from "../../base/slider/slider";
+export default {
+  data() {
+    return {
+      recommends: []
+    };
+  },
+  created() {
+    this._getRecommend();
+  },
+  components: {
+    Slider
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then(res => {
+        if (ERR_OK === res.code) {
+          this.recommends = res.data.slider;
+        }
+      });
     }
+  }
+};
 </script>
 
 <style lang="less" scoped>
+    @import "../../common/less/variable.less";
 
+    .recommend {
+        position: fixed;
+        width: 100%;
+        top: 88px;
+        bottom: 0
+    }
+    
+    .recommend-content {
+        height: 100%;
+        overflow: hidden;
+        .slider-wrapper {
+            position: relative;
+            width: 100%;
+            overflow: hidden;
+        }
+    }
+      
 </style>
